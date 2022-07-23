7710111df62de1014ebc18d13339809ac72911b7
FernandoCelmer <email@fernandocelmer.com>
2022-07-23 20:08:37 -0300

    No Task - Linux Profile Update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import typer
+from linux_profile.commands import Init
 
 app = typer.Typer(help="Awesome CLI prifile manager.")
 
 
 @app.command()
 def init(user: str, token: str):
-    print("init")
+    start = Init(user, token)
+    start.add_user()
 
 @app.command()
 def sync():
