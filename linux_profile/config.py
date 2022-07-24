ac63f1ad6e26fad4dc84f408031fd11772e2415f
FernandoCelmer <email@fernandocelmer.com>
2022-07-24 03:30:52 -0300

    No Task - Loading environment variables

--- linux_profile/config.py
+++ linux_profile/config.py
@@ -1,3 +1,9 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL_API = getenv("URL_API")
 FILE_CONF = '.linuxprofile.ini'
 FILE_DISTRO = '.os-release'
 FILE_SYSTEM = '.hostnamectl'
