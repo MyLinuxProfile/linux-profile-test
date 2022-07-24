6a42e9c1f3f54e97b9242fd4bffce56b2ce6ca9a
FernandoCelmer <email@fernandocelmer.com>
2022-07-23 22:51:34 -0300

    No Task - Implementation of new actions

--- linux_profile/__main__.py
+++ linux_profile/__main__.py
@@ -1,4 +1,5 @@
 import typer
+from linux_profile import __logo__, __version__
 from linux_profile.actions import Init
 
 app = typer.Typer(help="Awesome CLI prifile manager.")
@@ -8,11 +9,24 @@ app = typer.Typer(help="Awesome CLI prifile manager.")
 def init(user: str, token: str):
     start = Init(user, token)
     start.add_user()
+    print(__logo__)
+    print("My Linux Profile: v" + str(__version__))
+    print("Docs: https://github.com/MyLinuxProfile/linux-profile-pypi/")
 
 
 @app.command()
-def sync():
-    print("sync")
+def sync_pull():
+    print("sync_pull")
+
+
+@app.command()
+def sync_push():
+    print("sync_push")
+
+
+@app.command()
+def sync_commit():
+    print("sync_commit")
 
 
 if __name__ == "__main__":
