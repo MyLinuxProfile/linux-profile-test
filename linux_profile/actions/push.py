4c724a82ab70115978c9a7b75d403b9b60d7df4b
FernandoCelmer <email@fernandocelmer.com>
2022-07-27 00:20:47 -0300

    No Task - Standardization of project commands

--- linux_profile/actions/push.py
+++ linux_profile/actions/push.py
@@ -1,4 +1,6 @@
 from linux_profile.base import BaseProfile
+from linux_profile.utils.text import text_command
+
 
 class Push(BaseProfile):
     """Start of settings
@@ -6,12 +8,20 @@ class Push(BaseProfile):
     def setup(self) -> None:
         """Initial setup
         """
+        if self.param not in ['all']:
+            raise Exception("Invalid parameter: " + self.param + " not exist!")
+
+        text_command(value='sync-push ' + self.param)
+        getattr(self, 'param_'+self.param)()
+
         try:
             self.load_config()
             self.load_profile()
         except Exception as error:
             print(error)
-            raise ValueError("It is not possible to load the basic settings.")
+            raise Exception("It is not possible to load the basic settings.")
 
-    def test(self):
-        print("----")
+    def param_all(self):
+        """Param All
+        """
+        pass
