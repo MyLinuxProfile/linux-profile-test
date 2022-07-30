870b2912e5b22d6ba6f880dccaf209f7ac1e266a
FernandoCelmer <email@fernandocelmer.com>
2022-07-29 21:26:39 -0300

    No Task - Create Flow

--- linux_profile/actions/pull.py
+++ linux_profile/actions/pull.py
@@ -1,7 +1,6 @@
-import re
 from linux_profile.base import BaseProfile
 from linux_profile.utils.request import BaseRequest
-from linux_profile.utils.text import text_command
+from linux_profile.utils.text import text_command, text_question, text_info
 
 
 class Pull(BaseProfile):
@@ -21,7 +20,33 @@ class Pull(BaseProfile):
             raise Exception("Invalid parameter: " + self.param + " not exist!")
 
         text_command(value='sync-pull ' + self.param)
-        getattr(self, 'param_'+self.param)()
+        getattr(self, 'test_flow_'+self.param)()
+
+    def test_flow_all(self):
+        """
+        """
+        profiles = True
+
+        text_info(desc='Connect to the server to get the profile')
+
+        if profiles:
+            # TODO: fake_download_profile()
+            text_info(desc='Profile successfully updated!')
+        else:
+            text_question(value="You don't have profiles! Do you want to create? yes/no")
+            input_action = input()
+
+            while input_action not in ['yes', 'no']:
+                text_question(value="I need confirmation. yes/no?")
+                input_action = input()
+
+            if input_action == 'yes':
+                pass
+                # TODO: fake_create_profile()
+
+            if input_action == 'no':
+                pass
+                # TODO: fake_exit()
 
     def param_all(self):
         """Param All
