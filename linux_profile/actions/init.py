ceb2ef104478d76ef08367067443f2f6cd1a8bf3
FernandoCelmer <email@fernandocelmer.com>
2022-07-30 22:52:18 -0300

    No Task - Flow test

--- linux_profile/actions/init.py
+++ linux_profile/actions/init.py
@@ -12,8 +12,20 @@ class Init(BaseProfile):
         """Param Login
         """
         text_command(value='init ' + self.param)
+        # TODO: fake_login_user()
 
     def param_create(self):
         """Param Create
         """
         text_command(value='init ' + self.param)
+        # TODO: fake_create_user()
+
+    #####-----------FAKE------------#####
+
+    def fake_login_user(self):
+        pass
+
+    def fake_create_user(self):
+        pass
+
+    #####-----------FAKE------------#####
