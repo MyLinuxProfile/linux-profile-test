cea59ba6af4e50e0bfc2b4e8b40da174f0f33c1a
FernandoCelmer <email@fernandocelmer.com>
2022-07-31 23:27:18 -0300

    No Task - Specific class creation to orchestrate the commands in the terminal

---
+++ linux_profile/config/command.py
@@ -0,0 +1,23 @@
+from linux_profile.config.base import Config
+
+
+class BaseCommand(Config):
+    """Base Command class
+    """
+    PARAM = ['all']
+
+    def start(self):
+        """Start to basic settings
+        """
+        self.add_config()
+        self.load_config()
+        self.initial_commands()
+
+    def initial_commands(self) -> None:
+        """Initial Commands
+        """
+        if self.param:
+            if self.param not in self.PARAM:
+                raise Exception("Invalid parameter: " + self.param + " not exist!")
+
+            getattr(self, 'param_'+self.param)()
