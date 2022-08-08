6df1c106cd684f5e917665ac6022ba8eb4adbb8a
FernandoCelmer <email@fernandocelmer.com>
2022-08-08 00:22:37 -0300

    No Task - Removal of 'start' function

--- linux_profile/commands/pull.py
+++ linux_profile/commands/pull.py
@@ -7,14 +7,6 @@ from linux_profile.utils.text import text_command
 class Pull(BaseCommand):
     """Start of settings
     """
-    def start(self) -> None:
-        """Start
-        """
-        try:
-            self.load_config()
-            self.load_profile()
-        except Exception as error:
-            raise Exception("It is not possible to load the basic settings.") from error
 
     def param_all(self):
         """Param All
