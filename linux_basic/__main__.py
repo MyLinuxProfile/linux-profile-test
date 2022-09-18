24194aa77d7d9c48bf07d83df54db1bfa5c9e31b
FernandoCelmer <email@fernandocelmer.com>
2022-09-18 16:18:19 -0300

    No Task - Building very basic profile synchronization framework

--- linux_basic/__main__.py
+++ linux_basic/__main__.py
@@ -1,37 +1,14 @@
-import typer
+#!/usr/bin/env python
 
-from linux_basic.command import Init
-from linux_profile.utils.text import text_error, text_command
-
-app = typer.Typer(help="Awesome CLI prifile manager.")
+from setuptools import setup
+from command import Init, Add, Sync
 
 
-@app.command()
-def init(param: str):
-    try:
-        Init(param=param)
-    except Exception as error:
-        text_error(value=error.args[0])
-
-@app.command()
-def pull(param: str):
-    text_command(value='pull')
-
-
-@app.command()
-def push(param: str):
-    text_command(value='push')
-
-
-@app.command()
-def commit(param: str):
-    text_command(value='commit')
-
-
-@app.command()
-def apply(param: str):
-    text_command(value='apply')
-
-
-if __name__ == "__main__":
-    app()
+setup(
+        name="linux-profile",
+        cmdclass={
+                'init': Init,
+                'add': Add,
+                'sync': Sync
+            }
+    )
